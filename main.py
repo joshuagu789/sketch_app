b38e98421254ca3229e17ab4c1a48cfd2127ca5d
joshuagu789 <joshuagu789@gmail.com>
2024-04-29 17:17:19 -0400

    fixed gaussian diffusion and coding for training
    no change to gaussian diffusion class but changed how to create t for timesteps

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import torch
 import numpy as np
 from torchvision import transforms
 import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+
 from gaussian_diffusion import guassian_diffusion
 from u_net_no_conditioning import u_net_no_conditioning
 
 if __name__ == "__main__":
     
+    timesteps = 1000
+    batch_size = 64
+    learn_rate = 1e-3
+    epochs = 3
+
     image = load_image(
         "https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png"
     )
@@ -18,30 +25,37 @@ if __name__ == "__main__":
     image_as_tensor = image_as_tensor[None,:,:,:]
     image_as_tensor = image_as_tensor.to(torch.float32)
 
-    t = torch.linspace(0, 0, 1, dtype = torch.float32) # creates 1d tensor from start to end with num_timesteps entries
+    # t = torch.linspace(0, 0, 1, dtype = torch.float32) # creates 1d tensor from start to end with num_timesteps entries
+    t = torch.randint(0, timesteps, (batch_size,))
 
     true_noise = torch.randn(512,512,3)
     # plt.imshow(true_noise)
-    print(image_as_tensor)
+    # print(image_as_tensor)
     true_noise = true_noise[None,:,:,:]
     u_net = u_net_no_conditioning()
 
     # CUDA POWER
-    device = torch.device("cuda" if torch.cuda.is_available else "cpu")
-    print(f"using {device} device")
-    u_net = u_net.to(device)
+    # device = torch.device("cuda" if torch.cuda.is_available else "cpu")
+    # print(f"using {device} device")
+    # u_net = u_net.to(device)
     
-    u_net_tensor = u_net(image_as_tensor.cuda())
-    print(u_net.parameters)
-    u_net_image = u_net_tensor[0].permute(1,2,0).cpu().detach().numpy()
-    print(u_net_image)
-    plt.imshow(u_net_image)
+    # u_net_tensor = u_net(image_as_tensor.cuda())
+    # print(u_net.parameters)
+    # u_net_image = u_net_tensor[0].permute(1,2,0).cpu().detach().numpy()
+    # print(u_net_image)
+    # plt.imshow(u_net_image)
 
-    # diffuser = guassian_diffusion(num_timesteps=1)
-    # TEST = diffuser.q_sample(image_as_tensor, t)
+    #dataloader = DataLoader(, batch_size = batch_size, shuffle=True)
 
-    # for i in range(20):
-    #     TEST = diffuser.q_sample(TEST, t)
+    print(t.size())
+    diffuser = guassian_diffusion(num_timesteps=timesteps)
 
-    # plt.imshow(TEST[0].permute(1,2,0))
+    TEST = diffuser.q_sample(image_as_tensor, t)
+    plt.imshow(TEST[0].permute(1,2,0))
+
+    for i in range(10):  
+        TEST = diffuser.q_sample(TEST, t)
+        plt.imshow(TEST[0].permute(1,2,0))
+
+    plt.imshow(TEST[0].permute(1,2,0))
     print("done")
--- trainer.py
+++ trainer.py
@@ -4,10 +4,12 @@ from torch.utils.data import DataLoader     # transforms data and labels into it
 from torchvision import datasets            # stores data and their labels (also contains fashion mnist data set)
 from torchvision.transforms import ToTensor # converts python pil image or n dimensional array into tensor of choice
 import matplotlib.pyplot as plt             # for seeing the images
+from torch.optim import Adam                # optimizer
+import torch.nn.functional as F
 
 class trainer:
     """
-    
+    kaggle datasets download -d iamsouravbanerjee/animal-image-dataset-90-different-animals
     """
 
     device = "cuda" if torch.cuda.is_available else "cpu"
@@ -19,5 +21,31 @@ class trainer:
     epochs = 5
     loss_function = nn.CrossEntropyLoss()  
 
-    def train_loop():
-       return
+    def train_loop(noise_predictor, dataloader, t, learn_rate, epochs):
+        """
+        Takes in the unet model and a dataset for training noise prediction
+
+        noise_predictor: the unet model created from u_net_no_conditioning.py
+        dataloader: dataset converted into pytorch Dataloader
+        t: for timesteps
+        learn_rate: how fast weights are adjusted
+        epochs: number of iterations of training
+        """
+        optimizer = Adam(noise_predictor.parameters(), lr=learn_rate)
+
+        for epoch in range(epochs):
+            for step, batch in enumerate(dataloader):
+                optimizer.zero_grad()
+
+                noise = torch.randn_like(batch)
+
+                predicted_noise = noise_predictor(batch)    # note: hugging face says to implement timesteps into unet
+                loss = F.l1_loss(noise, predicted_noise)     # using l1 loss
+
+                if step % 100 == 0:
+                    print("Loss: " + str(loss.item()))
+
+                loss.backward()
+                optimizer.step()
+                
+                return
