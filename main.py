265f83304be620db635ecae3f986c8f96233c11d
joshuagu789 <joshuagu789@gmail.com>
2024-04-29 20:11:36 -0400

    import dataset
    imported jpg's from kaggle dataset, stored them in 4d tensor

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import torch
 import numpy as np
 from torchvision import transforms
 import matplotlib.pyplot as plt
+
 from torch.utils.data import DataLoader
+import opendatasets as od
+import os
+import glob
 
 from gaussian_diffusion import guassian_diffusion
 from u_net_no_conditioning import u_net_no_conditioning
@@ -15,12 +19,39 @@ if __name__ == "__main__":
     batch_size = 64
     learn_rate = 1e-3
     epochs = 3
+    rawdataset = []
+    pil_to_tensor = transforms.ToTensor()
+    resizer = transforms.Resize((512,512))
+    counter = 0
+    # for filename in glob.glob("C:\Users\joshu\Documents\GitHub\sketch_app/*.jpg"):
+    # for filename in glob.glob("./animal-image-dataset-90-different-animals/animals/*.jpg"):
+    for directory_name in os.listdir("animal-image-dataset-90-different-animals/animals/animals"):
+        for filename in glob.glob(os.path.join("animal-image-dataset-90-different-animals/animals/animals/" + str(directory_name),"*.jpg")):
+            # if counter <= 50:
+            img = Image.open(filename)
+            image_as_tensor = pil_to_tensor(img)
+            image_as_tensor = resizer(image_as_tensor)
+            image_as_tensor = image_as_tensor.to(torch.float32)
+            rawdataset.append(image_as_tensor)
+            print(counter)
+            counter += 1
+    print(len(rawdataset))
 
+    tensordataset = torch.stack(rawdataset)    # from array of 3d tensors into 4d tensor
+    print(tensordataset.size())
+    torch.save(tensordataset, "ninety-animals.pt")
+    plt.imshow(tensordataset[541].permute(1,2,0).cpu().detach().numpy())
+    plt.imshow(tensordataset[1331].permute(1,2,0).cpu().detach().numpy())
+    plt.imshow(tensordataset[3911].permute(1,2,0).cpu().detach().numpy())
+    plt.imshow(tensordataset[4912].permute(1,2,0).cpu().detach().numpy())
+
+    od.download(
+        "https://www.kaggle.com/datasets/iamsouravbanerjee/animal-image-dataset-90-different-animals"
+    )
     image = load_image(
         "https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png"
     )
 
-    pil_to_tensor = transforms.ToTensor()
     image_as_tensor = pil_to_tensor(image)
     image_as_tensor = image_as_tensor[None,:,:,:]
     image_as_tensor = image_as_tensor.to(torch.float32)
